8e8a3a7161e716afc21e14dbb1e3b1d93e1eb112
denniseccher <denniseccher@gmail.com>
2025-05-01 22:00:59 +0200

    added courses service

---
+++ lib/api/services/api.course.service.dart
@@ -0,0 +1,42 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:miss_minutes/api/classes/api.course.class.dart';
+import 'package:miss_minutes/api/utilities/api.collections.utility.dart';
+
+class ApiCourseService{
+  final ApiCollectionsUtility _apiCollectionsUtility = ApiCollectionsUtility();
+  final User? user = FirebaseAuth.instance.currentUser;
+
+  Future<List<ApiCourse>> getCourses() async{
+    QuerySnapshot snapshot = await _apiCollectionsUtility.coursesRef
+      .where("uid", isEqualTo: user?.uid)
+      .get();
+    List<QueryDocumentSnapshot<Object?>> docs = snapshot.docs;
+
+    return docs.map(
+      (doc){
+        try {
+          Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+          data['id'] = doc.id;
+          return ApiCourse.fromJson(data);
+        } catch (e) {
+          return null;
+        }
+      }
+    ).where((element) => element != null).cast<ApiCourse>().toList();
+  }
+
+  void addCourse(ApiCourse newCourse){
+    _apiCollectionsUtility.coursesRef.add(newCourse.toJson());
+  }
+
+  void deleteCourse(String id){
+    _apiCollectionsUtility.coursesRef.doc(id).delete();
+  }
+
+  void updateCourse(ApiCourse newCourse){
+    _apiCollectionsUtility.shiftsRef.doc(newCourse.id).update(
+      newCourse.toJson()
+    );
+  }
+}
--- lib/api/utilities/api.collections.utility.dart
+++ lib/api/utilities/api.collections.utility.dart
@@ -3,4 +3,5 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class ApiCollectionsUtility{
   CollectionReference optionsRef = FirebaseFirestore.instance.collection('options');
   CollectionReference shiftsRef = FirebaseFirestore.instance.collection('shifts');
+  CollectionReference coursesRef = FirebaseFirestore.instance.collection('courses');
 }
